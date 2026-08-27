64a55e4e926e17062f6752ac99bd519d4858f47c
Cayo Nunes <cayocesard@gmail.com>
2026-08-27 18:16:57 -0300

    refactor: simplifica layout da mini bio, utiliza foto-cayo.jpeg e ajusta para um tom mais humanizado

--- mini-bio/components/Profile.js
+++ mini-bio/components/Profile.js
@@ -1,18 +1,19 @@
+
 import Image from 'next/image';
 
-export default function Profile({ 
-  name = "Cayo", 
-  role = "Desenvolvedor Frontend", 
-  photoUrl = "/avatar.jpg" 
+export default function Profile({
+  name = "Cayo",
+  role = "Desenvolvedor Frontend",
+  photoUrl = "/avatar.jpg"
 }) {
   return (
     <div className="profile-container">
       <span className="component-badge">Profile.js</span>
-      
+
       {/* 1. Sua Foto */}
       <div className="profile-avatar-wrapper">
-        <Image 
-          src={photoUrl} 
+        <Image
+          src={photoUrl}
           alt={`Foto de ${name}`}
           width={120}
           height={120}
