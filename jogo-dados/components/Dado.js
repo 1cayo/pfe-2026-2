794746634613a5fe74b9ccac8e80f98ef787a266
Cayo Nunes <cayocesard@gmail.com>
2026-09-14 15:01:39 -0300

    adiciona imagens PNG dos dados de 0 a 6 em public/dados

--- jogo-dados/components/Dado.js
+++ jogo-dados/components/Dado.js
@@ -1,10 +1,10 @@
 ﻿export default function Dado({ valor }) {
-  // Se valor não for passado ou for 0, mostra o dado em branco/placeholder (dado0.svg)
+  // Se valor não for informado ou for 0, usa a imagem do placeholder dado0.png
   const dadoValor = valor || 0;
 
   return (
     <img
-      src={`/dados/dado${dadoValor}.svg`}
+      src={`/dados/dado${dadoValor}.png`}
       alt={`Dado ${dadoValor}`}
       width={75}
       height={75}
